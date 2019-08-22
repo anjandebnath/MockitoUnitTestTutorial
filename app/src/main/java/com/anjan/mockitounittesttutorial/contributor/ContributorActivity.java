f6af6bee5af9798526ef874b74079d7abadc7004
Anjan Debnath <anjan0151@gmail.com.com>
2019-08-22 13:51:07 +0600

    Method added by owner

--- app/src/main/java/com/anjan/mockitounittesttutorial/contributor/ContributorActivity.java
+++ app/src/main/java/com/anjan/mockitounittesttutorial/contributor/ContributorActivity.java
@@ -3,6 +3,7 @@ package com.anjan.mockitounittesttutorial.contributor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.anjan.mockitounittesttutorial.R;
 
@@ -22,6 +23,11 @@ public class ContributorActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+    }
+
+    public void methodCreatedByOwner(){
+
+        Log.d("Owner", "This method has created by owner");
     }
 }
 
