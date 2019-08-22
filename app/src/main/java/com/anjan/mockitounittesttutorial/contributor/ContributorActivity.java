b7ec6984e1a6fae6f1a4e386058bf40fdb580e73
Anjan Debnath <anjan0151@gmail.com.com>
2019-08-22 12:20:51 +0600

    Contributor Activity method added

--- app/src/main/java/com/anjan/mockitounittesttutorial/contributor/ContributorActivity.java
+++ app/src/main/java/com/anjan/mockitounittesttutorial/contributor/ContributorActivity.java
@@ -1,4 +1,28 @@
 package com.anjan.mockitounittesttutorial.contributor;
 
-public class ContributorActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.anjan.mockitounittesttutorial.R;
+
+public class ContributorActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
 }
+
+
