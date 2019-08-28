e56b1395922235972bff7d82b0c027ed5536e1c2
Anjan Debnath <anjan0151@gmail.com.com>
2019-08-28 13:31:41 +0600

    new method added

--- app/src/main/java/com/anjan/mockitounittesttutorial/Locator.java
+++ app/src/main/java/com/anjan/mockitounittesttutorial/Locator.java
@@ -23,5 +23,18 @@ public class Locator {
         }
     }
 
+    public static MessagingService getService(String serviceName) {
 
+        MessagingService service = cache.getService(serviceName);
+
+        if (service != null) {
+            return service;
+        }
+
+        InitialContext context = new InitialContext();
+        MessagingService service1 = (MessagingService) context
+                .lookup(serviceName);
+        cache.addService(service1);
+        return service1;
+    }
 }
